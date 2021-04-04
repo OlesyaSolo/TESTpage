7180718a4c0a35d53c223cdd0ac4696d5169373d
Olesya Solovyeva <lazylesi@mail.ru>
2021-04-04 20:52:13 +0300

    Создание приложения

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+tasks_method = ['play_game', 'sleep', 'drink_wine']
+
+@app.route("/")
+@app.route("/list")
+def items():  
+    return render_template("list.html", 
+                           tasks=tasks_method)
+
+
+
+@app.route("/add", methods=['POST', 'GET'])
+def add():
+    if request.method == 'GET':
+        return render_template("add.html")
+    else:
+        task_body = request.form.get('task_body', None)
+        if task_body:
+            tasks_method.append(task_body)
+        return items()
+
+if __name__ == "__main__":
+    app.run(host="localhost", port="5000", debug=True)
+
